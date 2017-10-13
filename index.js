92553ddbee189251bd633a15300506b3064e09f6
Kyrill Tircher <kyrilltircher14@hotmail.com>
2017-10-13 16:42:11 +0200

    Base Websockets

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/www')); //dossier public
+//www is the public directory served to clients
+app.use(express.static(__dirname + '/www'));
 
-
+//get at root
 app.get('/', function(req, res) {
 	res.sendFile('www/index.html');
 });
 
-app.listen(app.get('port'), function() {
-	console.log("Pac man is listening on port " + app.get('port'));
+//socket managing
+io.on('connection', function(socket) {
+	console.log("A websocket connected");
+	socket.on('disconnect', function() {
+		console.log("A websocket disconnected");
+	});
+});
+
+server.listen(app.get('port'), function() {
+	console.log("Pacman is listening on port " + app.get('port'));
 });
