9f2d4c93b5d428f0dcdf2fcd5cfcc52da751b520
kyky <kyrilltircher14@hotmail.com>
2017-11-08 16:00:18 +0100

    remove menu for debug mobile

--- www/js/phaserStates/gameBoot.js
+++ www/js/phaserStates/gameBoot.js
@@ -1,6 +1,7 @@
 var bootState = {
 	create: function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.state.start('titleMenuState');
+		//game.state.start('titleMenuState');
+		game.state.start('defaultPacman');
 	}
 }
