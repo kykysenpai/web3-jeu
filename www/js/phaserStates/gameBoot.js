eaf3d08ba03739faad607018fbdce3be7dd68ff9
Kyrill Tircher <kyrilltircher14@hotmail.com>
2017-11-08 18:13:25 +0100

    fin Debug

--- www/js/phaserStates/gameBoot.js
+++ www/js/phaserStates/gameBoot.js
@@ -2,6 +2,6 @@ var bootState = {
 	create: function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		//game.state.start('titleMenuState');
-		game.state.start('defaultPacman');
+		game.state.start('titleMenuState');
 	}
 }
