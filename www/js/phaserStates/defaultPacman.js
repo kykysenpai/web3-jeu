7201272dcb637531264a5fd26d09337417beaa75
dieBrouzouf <debroux.michell@gmail.com>
2017-11-10 13:28:51 +0100

    l'update du lag

--- www/js/phaserStates/defaultPacman.js
+++ www/js/phaserStates/defaultPacman.js
@@ -98,7 +98,7 @@ var defaultPacman = {
 		this.scoresDisplay.position.x = game.width / 2;
 		//this.scoresDisplay.setTextBounds(0, 0, 400, 0);
 		this.scoresDisplay.fixedToCamera = true;
-		this.map.createFromTiles(this.safetile, this.safetile, 'dot', this.layer, this.dots);
+		//this.map.createFromTiles(this.safetile, this.safetile, 'dot', this.layer, this.dots);
 		this.world.setBounds(0, 0, 1920, 1920);
 		//  The dots will need to be offset by 6px to put them back in the middle of the grid => I trust the dude from the tutorial lmao
 		this.dots.setAll('x', 6, false, false, 1);
@@ -271,6 +271,14 @@ var defaultPacman = {
 		newPlayer.body.setSize(16, 16, 0, 0);
 		newPlayer.play('munch');
 		this.players[data.playerId] = newPlayer;
+	},
+	//instanciate a dot
+	createDot:function(data){
+		var newDot = this.add.sprite(data.x, data.y, 'dot');
+		if (!data.isAlive){
+			newDot.visible = false;
+		}
+		return newDot;
 	},
 	checkKeys: function() {
 
@@ -466,7 +474,7 @@ var defaultPacman = {
 		if (this.updateNeeded == (theoreticalFps / howManyInfoPerSecond)) {
 			this.updateNeeded = 0;
 			this.positionUpdate();
-			console.log("x : " + this.pacman.x + " , y : " + this.pacman.y);
+			//console.log("x : " + this.pacman.x + " , y : " + this.pacman.y);
 		}
 	}
 }
@@ -495,6 +503,7 @@ function defaultPacmanSockets() {
 
 	//Getting all currently connected player
 	socket.on('users', function(data) {
+		console.log(data);
 		game.state.callbackContext.playerId = data.playerId;
 		for (var player in data.players) {
 			if (player === data.playerId) {
@@ -503,14 +512,11 @@ function defaultPacmanSockets() {
 			}
 			game.state.callbackContext.createPlayer(data.players[player]);
 		}
-		this.mapDots = [];
+		game.state.callbackContext.mapDots = [];
 		for(var i in data.mapDots){
 			var dot = data.mapDots[i];
-			var spriteDot = new Sprite(game, dot.x, dot.y, 'dot', i);
-			if (!dot.isAlive){
-				spriteDot.visible = false;
-			}
-			this.mapDots.push(spriteDot);
+			var spriteDot = game.state.callbackContext.createDot(dot);
+			game.state.callbackContext.mapDots.push(spriteDot);
 		}
 	});
 
@@ -547,12 +553,16 @@ function defaultPacmanSockets() {
 		game.state.callbackContext.updateScores(infos.scores);
 
 		//info dots
-		dots = infos.dots;
-		for(var i in dots){
-			if (dots[i].isAlive){
-				this.mapDots[i].visible = true;
-			} else {
-				this.mapDots[i].visible = false;
+		if(game.state.callbackContext.mapDots != null){
+			dots = infos.dots;
+			
+			console.log(dots);
+			for(var i in dots){
+				if (dots[i].isAlive){
+					game.state.callbackContext.mapDots[i].visible = true;
+				} else {
+					game.state.callbackContext.mapDots[i].visible = false;
+				}
 			}
 		}
 	});
@@ -566,4 +576,5 @@ function defaultPacmanSockets() {
 		y: game.state.callbackContext.pacman.y,
 		dir: game.state.callbackContext.current
 	});
+	console.log("lel first emit");
 };
