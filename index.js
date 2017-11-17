9798d62a71d72c736f201c42e66a333284ca436d
dieBrouzouf <debroux.michell@gmail.com>
2017-11-17 14:00:22 +0100

    spawnpoint

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var options = {index: "./index.js"};
 //app.use('/', express.static('app', options));
 app.use(express.static(__dirname + '/www'))
 
-require('./modules/MapGenerator.js');
+//require('./modules/MapGenerator.js');
 
 app.set('port', process.env.PORT || 5000);
 server.listen(app.get('port'), function() {
