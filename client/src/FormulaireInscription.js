f12cfe4073dd6dcb0f583bcbd15924761f960ef7
SebbyBE <sebastien.place@yahoo.fr>
2017-11-30 16:44:42 +0100

    modify some shit in react-redux

--- client/src/FormulaireInscription.js
+++ client/src/FormulaireInscription.js
@@ -1,6 +1,27 @@
 import React,{Component} from 'react';
 
+//import {connect} from 'react-redux';
+
 class FormulaireInscription extends Component {
+    constructor(props){
+        super(props);
+        this.state={id:"",mdp:""};
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChangeId = this.handleChangeId.bind(this);
+        this.handleChangeMdp = this.handleChangeMdp.bind(this);
+    }
+    handleClick = (ev) =>{
+        ev.preventDefault();
+        console.log("je dois encore faire des trucs");
+        console.log(this.state.id);
+        console.log(this.state.mdp);
+    }
+    handleChangeId(ev){
+        this.setState({id:ev.target.value});
+    }
+    handleChangeMdp(ev){
+        this.setState({mdp:ev.target.value});
+    }
     render(){
         return(
             <div id="signup">
@@ -11,16 +32,16 @@ class FormulaireInscription extends Component {
 
                 <div className="row">
                     <label>Pseudo<span className="req">*</span></label>
-                    <input type="text" required autoComplete="off" id="pseudoInscription" />
+                    <input type="text" required autoComplete="off" id="pseudoInscription" value={this.state.id} onChange={this.handleChangeId}/>
                 </div>
 
                 <div className="row">
                     <label>Mot de passe<span className="req">*</span></label>
-                    <input type="password" required autoComplete="off" id="mdpInscription" />
+                    <input type="password" required autoComplete="off" id="mdpInscription" value={this.state.mdp} onChange={this.handleChangeMdp}/>
                 </div>
 
                 <div className="row">
-                    <button type="submit" className="button button-block" id="sInscrire" >C'est parti!</button>
+                    <button type="submit" className="button button-block" id="sInscrire" onClick={this.handleClick}>C'est parti!</button>
                 </div>
 
                 </div>
