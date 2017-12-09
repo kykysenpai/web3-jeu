2b2e7520a561689814df1263b2f7176fcd898008
Kyrill Tircher <kyrilltircher14@hotmail.com>
2017-12-09 22:04:26 +0100

    bug of randomMap

--- client/public/js/phaserStates/selectPlayer.js
+++ client/public/js/phaserStates/selectPlayer.js
@@ -10,57 +10,9 @@ var selectPlayer = {
 		game.load.image('start', 'assets/start.png');
 		for (var i = 0; i < skinList.length; i++) {
 			this.load.spritesheet(skinList[i].name, skinList[i].path, 32, 32);
-			console.log(skinList[i].name + "\n" + skinList[i].path)
 		}
-		this.skinFrameP = this.add.group();
-		this.skinFrameG = this.add.group();
 	},
-	/*addSkin: function(group, x, y, skinName) {
-		return group.create(x, y, skinName);
-	},*/
 	create: function() {
-		var game = this;
-		$.ajax({
-			url: '/infoPlayer',
-			type: 'POST',
-			data: {
-				authName: sessionStorage.getItem("authName") ? sessionStorage.getItem("authName") : localStorage.getItem("authName")
-			},
-			success: function(ret) {
-				console.log(ret);
-				/*
-				game.skinFrameP = game.add.image(72, 270, 'skinFrame');
-				game.skinFrameG = game.add.image(72, 270, 'skinFrame');
-				*/
-				var j = 80;
-				for (var skinP in ret["pacmanSkins"]) {
-					//var thumb = game.addSkin(game.skinFrameP, j, 270, ret["pacmanSkins"][skinP]);
-					var thumb = game.skinFrameP.create(j, 270, ret["pacmanSkins"][skinP]);
-					j += 37; //32px for sprite width + 5 for margin
-					thumb.skin = ret["pacmanSkins"][skinP];
-					thumb.inputEnabled = true;
-					thumb.events.onInputDown.add(function(clickedImage) {
-						playerInfos.skin = clickedImage.skin;
-					}, this);
-
-				}
-				var j = 80;
-				for (var skinP in ret["ghostSkins"]) {
-					//var thumb = game.addSkin(game.skinFrameG, j, 270, ret["ghostSkins"][skinP]);
-					var thumb = game.skinFrameG.create(j, 270, ret["ghostSkins"][skinP]);
-					j += 37; //32px for sprite width + 5 for margin
-					thumb.skin = ret["ghostSkins"][skinP];
-					thumb.inputEnabled = true;
-					thumb.events.onInputDown.add(function(clickedImage) {
-						playerInfos.skin = clickedImage.skin;
-					}, this);
-				}
-				//load les skins :D
-			},
-			error: function(ret) {
-				console.log(ret);
-			}
-		});
 		var teamHeight = 160;
 		var bg = game.add.image(0, 0, 'bg');
 		var title = game.add.image(0, 10, 'title');
@@ -86,8 +38,6 @@ var selectPlayer = {
 		team1.input.useHandCursor = true;
 		team1.events.onInputDown.add(function(clickedTeam) {
 			playerInfos.team = clickedTeam.team;
-			this.skinFrameG.visible = false;
-			this.skinFrameP.visible = true;
 			team2.alpha = 0.5;
 			team1.alpha = 1;
 		}, this);
@@ -100,14 +50,22 @@ var selectPlayer = {
 		team2.inputEnabled = true;
 		team2.input.useHandCursor = true;
 		team2.events.onInputDown.add(function(clickedTeam) {
-			this.skinFrameP.visible = false;
-			this.skinFrameG.visible = true;
 			playerInfos.team = clickedTeam.team;
 			team1.alpha = 0.5;
 			team2.alpha = 1;
 		}, this);
 
+		var skinFrame = game.add.image(72, 270, 'skinFrame');
 
-
+		var j = 80;
+		for (var i = 0; i < skinList.length; i++) {
+			var thumb = game.add.sprite(j, 270, skinList[i].name, 0);
+			j += 37; //32px for sprite width + 5 for margin
+			thumb.skin = skinList[i].name;
+			thumb.inputEnabled = true;
+			thumb.events.onInputDown.add(function(clickedImage) {
+				playerInfos.skin = clickedImage.skin;
+			}, this);
+		}
 	}
 }
