2b7e7be9c20b220dbd7c824ef4e5af5d0a6cac28
SebbyBe <sebastien.place@yahoo.fr>
2017-12-10 20:25:02 +0100

    ajout de toutes les incones pour des partage, on peut encore en ajouter. ALEX LES DOSSIERS QUE TU DOIS AVOIR DANS BUILD TU DOIS AUSSI LES AVOIR DANS PUBLIC >< j'ai mis 10 minutes à savoir pourquoi ça crachait non stop x)

--- client/src/Profil/ProfilButtonShareFb.js
+++ client/src/Profil/ProfilButtonShareFb.js
@@ -1,22 +1,107 @@
 import React, { Component } from 'react';
+import {ShareButtons,generateShareIcon} from 'react-share';
+
+const {
+    FacebookShareButton,
+    GooglePlusShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    RedditShareButton,
+    EmailShareButton,
+  } = ShareButtons;
+  
+  const FacebookIcon = generateShareIcon('facebook');
+  const TwitterIcon = generateShareIcon('twitter');
+  const GooglePlusIcon = generateShareIcon('google');
+  const WhatsappIcon = generateShareIcon('whatsapp');
+  const RedditIcon = generateShareIcon('reddit');
+  const EmailIcon = generateShareIcon('email');
 
 class ProfilButtonShareFb extends Component{
-    constructor(props){
-        super(props);
-        
-
-        this.partager = this.partager.bind(this);
-    }
-
-    partager = () =>{
-        var shareUrl = "https://facebook.com/sharer/sharer.php?u=" + window.location;
-        window.open(shareUrl,"Share dat shit motherfucker","height=200,width=150");
-    }
-
     render(){
+        var shareUrl = "" + window.location;
         return(
             <div className="row">
-                <button className="button button-block" onClick={this.partager}>Partager</button>
+
+            <div className="someNetwork">
+             <FacebookShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            hashtag="Pacman Evolution"
+            tabIndex="0">
+            <FacebookIcon
+              size={64}
+              round />
+            </FacebookShareButton>
+            </div>
+
+            <div className="someNetwork">
+            <TwitterShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            tabIndex="1"
+            title="Pacman Evolution"
+            via="PasEncoreDeCompteTwitter">
+            <TwitterIcon
+              size={64}
+              round />
+            </TwitterShareButton>
+            </div>
+
+            <div className="someNetwork">
+            <GooglePlusShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            tabIndex="2">
+            <GooglePlusIcon
+              size={64}
+              round />
+            </GooglePlusShareButton>
+            </div>
+
+            <div className="someNetwork">
+            <WhatsappShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            tabIndex="4"
+            title="Pacman Evolution"
+            separation=" ">
+            <WhatsappIcon
+              size={64}
+              round />
+            </WhatsappShareButton>
+            </div>
+
+            <div className="someNetwork">
+            <RedditShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            tabIndex="5"
+            title="Pacman Evolution">
+            <RedditIcon
+              size={64}
+              round />
+            </RedditShareButton>
+            </div>
+
+            <div className="someNetwork">
+            <EmailShareButton
+            url={shareUrl}
+            quote={"Viens jouez!!!"}
+            className="share-button col-xs-1 col-sm-1 col-md-2 col-lg-2"
+            tabIndex="6"
+            subject="Pacman Evolution"
+            body={"Viens jouer sur: "+window.location}>
+            <EmailIcon
+              size={64}
+              round />
+            </EmailShareButton>
+            </div>
             </div>
         );
     }
