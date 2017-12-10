64cf4e8fe3bdfa38aa6f2c76c78061291de499a4
Kyrill Tircher <kyrilltircher14@hotmail.com>
2017-12-10 22:02:58 +0100

    oAuth

--- modules/oAuth.js
+++ modules/oAuth.js
@@ -1,9 +1,9 @@
 var id = {
-    facebook: {
-        clientID: 264160964107154,
-        clientSecret: 'd8593533f8001ae888d151fb56988bde',
-        callbackURL: 'http://localhost:5000/auth/facebookConnect/callback'
-    }
+	facebook: {
+		clientID: 264160964107154,
+		clientSecret: 'd8593533f8001ae888d151fb56988bde',
+		callbackURL: 'http://pacman-web3.tircher.be/auth/facebookConnect/callback'
+	}
 };
 
 module.exports = id;
